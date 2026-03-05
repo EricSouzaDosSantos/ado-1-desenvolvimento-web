39837de66546c1bd8a3a47e96b1d6f6e0921a56a
Eric <ericsouza94895@gmail.com>
2026-03-04 21:45:46 -0300

    feat: add servelet class

---
+++ src/main/java/br/com/carstore/servlet/CreateItemServlet.java
@@ -0,0 +1,13 @@
+package br.com.carstore.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-item")
+public class CreateItemServlet extends HttpServlet {
+
+}
